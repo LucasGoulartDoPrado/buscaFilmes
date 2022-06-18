70e8518aac512a113058932babedb832e614f3e4
Lucas Goulart do Prado <34442250+LucasGoulartDoPrado@users.noreply.github.com>
2022-06-18 15:20:07 -0300

    Varrer Site de Filmes
    Varrer Site de Filmes e armazenar as informações em variaveis e exportação para json e csv.

---
+++ busca_filmes/busca_filmes/spiders/buscafilmes.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class BuscafilmesSpider(scrapy.Spider):
+    name = 'buscafilmes'
+    # allowed_domains = ['imdb.com']
+    start_urls = ['https://www.imdb.com/chart/top/']
+
+    def parse(self, response):
+        for filmes in response.css('.titleColumn'):
+            yield{
+            'nome': filmes.css('.titleColumn a::text').get(),
+            'ano' : filmes.css('.secondaryInfo ::text').get()[1:-1],
+            'nota' : response.css('strong::text').get()
+            }
+        pass
